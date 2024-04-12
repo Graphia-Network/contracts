43e74487f797bb8d6d67d85ed5cf016c063d8e62
Aleksa Milosevic <aleksa.milosevic@gmail.com>
2024-04-12 22:56:43 +0200

    Added docgen.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "solidity-docgen";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  docgen: {
+    outputDir: "docs",
+    theme: "markdown",
+  }
 };
 
 export default config;
