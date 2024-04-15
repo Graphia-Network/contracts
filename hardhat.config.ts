6185017ce08fd9790cb7248b1bd35b547b79d710
Aleksa Milosevic <aleksa.milosevic@gmail.com>
2024-04-15 18:11:33 +0200

    ABI exporter.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "solidity-docgen";
+import "hardhat-abi-exporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   docgen: {
     outputDir: "docs",
     theme: "markdown",
+  },
+  abiExporter: {
+    clear: true,
+    only: ['Assets'],
   }
 };
 
